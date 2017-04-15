a78a3ff07acf5048843b8ecefab4aed7624d779e
Marek Newton <marek.s.newton@gmail.com>
2017-04-15 13:53:02 -0600

    first

---
+++ Assets/scripts/EnemyCreator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCreator : MonoBehaviour {
+
+	public GameObject enemy;
+	public float speed;
+
+	private GameObject[] enemys;
+
+	void reset() {
+		for (int i = -4; i < 5; i++) {
+			Instantiate (enemy, new Vector3 (i, 1, 4), Quaternion.identity);
+		}
+	}
+	
+	void Start () {
+		reset ();
+	}
+
+	void Update () {
+		enemys = GameObject.FindGameObjectsWithTag ("Enemy");
+
+		for (int i = 0; i < enemys.Length; i++) {
+
+			float x = enemys[i].transform.position.x;
+			float y = enemys[i].transform.position.y;
+			float z = enemys[i].transform.position.z;
+
+			enemys [i].transform.position = new Vector3 (x, y, z - speed);
+
+			if (z < -10) {
+				Destroy (enemys [i]);
+				reset ();
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed;
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidBody.AddForce (movement * speed);
+	}
+}
